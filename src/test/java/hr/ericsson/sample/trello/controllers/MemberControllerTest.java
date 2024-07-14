25bdb1e19780a2b5f3a64e9490b5622138a129f4
Domagoj Borovcak <borovcakdomagoj@gmail.com>
2024-07-14 14:19:37 +0200

    add some test plus small fixes

--- src/test/java/hr/ericsson/sample/trello/controllers/MemberControllerTest.java
+++ src/test/java/hr/ericsson/sample/trello/controllers/MemberControllerTest.java
@@ -4,12 +4,13 @@ import hr.ericsson.sample.trello.TestBase;
 import hr.ericsson.sample.trello.controllers.response.MemberResponse;
 import hr.ericsson.sample.trello.controllers.response.MembersResponse;
 import hr.ericsson.sample.trello.fixtures.MembersFixture;
-import hr.ericsson.sample.trello.repositories.models.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,7 +46,6 @@ class MemberControllerTest extends TestBase {
         //given
         memberRepository.save(MembersFixture.getMember().build());
         memberRepository.save(MembersFixture.getMember()
-                .id(2L)
                 .name("Doe")
                 .lastName("John")
                 .email("doe.john@email.com")
@@ -83,19 +83,75 @@ class MemberControllerTest extends TestBase {
             """)
     void shouldReturnMemberByIdWithStatusCodeOK() throws Exception {
         //given
-        memberRepository.save(MembersFixture.getMember().build());
+        var id = memberRepository.save(MembersFixture.getMember().build()).getId();
         memberRepository.save(MembersFixture.getMember()
-                .id(2L)
                 .name("Doe")
                 .lastName("John")
                 .email("doe.john@email.com")
                 .phone("412587747")
                 .build());
         //when
-        var firstId = memberRepository.findAll().stream().map(Member::getId).findFirst().orElseThrow();
-        var result = mockMvc.perform(get("/v1/members/" + firstId))
+        var result = mockMvc.perform(get("/v1/members/" + id))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        //then
+        var membersResponse = objectMapper.readValue(result, MemberResponse.class);
+        assertThat(membersResponse.name()).isEqualTo("John");
+        assertThat(membersResponse.lastName()).isEqualTo("Doe");
+        assertThat(membersResponse.email()).isEqualTo("john.doe@example.com");
+        assertThat(membersResponse.phone()).isEqualTo("125652547");
+    }
+
+    @Test
+    @DisplayName("""
+            Given multiple members to database,
+             when fetching member by particular non existing id,
+             then is expected to return Not found exception with status code Not Found
+            """)
+    void shouldReturnNotFoundExceptionByNonExistingIdWithStatusCodeNotFound() throws Exception {
+        //given
+        var id = memberRepository.save(MembersFixture.getMember().build()).getId();
+        memberRepository.save(MembersFixture.getMember()
+                .name("Doe")
+                .lastName("John")
+                .email("doe.john@email.com")
+                .phone("412587747")
+                .build());
+        var nonExistingId = id + 10;
+        //when
+        var result = mockMvc.perform(get("/v1/members/" + nonExistingId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        //then
+        var membersResponse = objectMapper.readValue(result, ProblemDetail.class);
+        assertThat(membersResponse.getType()).hasToString("about:blank");
+        assertThat(membersResponse.getTitle()).isEqualTo("Not Found");
+        assertThat(membersResponse.getStatus()).isEqualTo(404);
+        assertThat(membersResponse.getDetail())
+                .contains("Could not find member by this id " + nonExistingId);
+        assertThat(membersResponse.getInstance()).hasToString("/v1/members/" + nonExistingId);
+    }
+
+    @Test
+    @DisplayName("""
+            Given new member request does not exist in database,
+             when new member request is sent, validated and all passed well
+             then new member is created and expected to return status code created
+            """)
+    void shouldReturnNewCreatedMemberWithStatusCodeCreated() throws Exception {
+        //given
+        var addMemberRequest = MembersFixture.addMemberRequestBuilder().build();
+        //when
+        var result = mockMvc.perform(post("/v1/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(addMemberRequest)))
+                .andExpect(status().isCreated())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
--- src/test/java/hr/ericsson/sample/trello/fixtures/MembersFixture.java
+++ src/test/java/hr/ericsson/sample/trello/fixtures/MembersFixture.java
@@ -1,5 +1,6 @@
 package hr.ericsson.sample.trello.fixtures;
 
+import hr.ericsson.sample.trello.controllers.request.AddMemberRequest;
 import hr.ericsson.sample.trello.repositories.models.Member;
 
 public class MembersFixture {
@@ -8,7 +9,14 @@ public class MembersFixture {
 
     public static Member.MemberBuilder getMember() {
         return Member.builder()
-                .id(1L)
+                .name("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .phone("125652547");
+    }
+
+    public static AddMemberRequest.AddMemberRequestBuilder addMemberRequestBuilder(){
+        return AddMemberRequest.builder()
                 .name("John")
                 .lastName("Doe")
                 .email("john.doe@example.com")
